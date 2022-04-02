085b4d6625e284c074756e4bcf0c5d4d64723a51
AnnaBurdys <annaburdy@gmail.com>
2022-04-02 19:31:25 +0300

    Solved the problems with arrays

--- src/by/kursy/annaburdys/javatasks/Classwork/Stage11/Task02/controller/Main.java
+++ src/by/kursy/annaburdys/javatasks/Classwork/Stage11/Task02/controller/Main.java
@@ -8,13 +8,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Printer.print("Input marks\n ");
-            int mark1 = scanner.nextInt();
-            int mark2 = scanner.nextInt();
-            int mark3 = scanner.nextInt();
-            int mark4 = scanner.nextInt();
-            int mark5 = scanner.nextInt();
-        double avg = Manager.countMark(mark1, mark2,mark3,mark4,mark5);
+        int size = scanner.nextInt();
+
+        Printer.print("Input count of students\n ");
+        int count = scanner.nextInt();
+
+        int[] marks = new int[count];   //definition
+
+        Printer.print("Input marks: ");
+        for (int i = 0; i< marks.length; i++) {
+            marks[i] = scanner.nextInt();
+        }
+        double avg = Manager.countMark(marks);
 
         String result = String.format("\nStudent's average mark = %.2f", avg);
         Printer.print(result);
--- src/by/kursy/annaburdys/javatasks/Classwork/Stage11/Task02/model/Manager.java
+++ src/by/kursy/annaburdys/javatasks/Classwork/Stage11/Task02/model/Manager.java
@@ -2,8 +2,11 @@ package by.kursy.annaburdys.javatasks.Classwork.Stage11.Task02.model;
 
 public class Manager {
     public static final int MARK_COUNT = 5;
-    public static double countMark (int mark1, int mark2, int mark3, int mark4, int mark5) {
-        double sum = mark1+mark3+mark3+mark4+mark5;
-        return sum/MARK_COUNT;
+    public static double countMark (int[] marks) {
+        double sum = 0;
+        for (int i = 0; i< marks.length; i++) {
+            sum+= marks[i];
+        }
+        return sum;
     }
 }
