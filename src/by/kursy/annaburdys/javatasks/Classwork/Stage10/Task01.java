e26c4bed7572eff4b0052e81965c89c1ff756639
AnnaBurdys <annaburdys@gmail.com>
2022-04-14 21:10:33 +0300

    updated

--- src/by/kursy/annaburdys/javatasks/Classwork/Stage10/Task01.java
+++ src/by/kursy/annaburdys/javatasks/Classwork/Stage10/Task01.java
@@ -1,7 +1,6 @@
 package by.kursy.annaburdys.javatasks.Classwork.Stage10;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+
 
 public class Task01 {
     public static void main(String[] args) {
